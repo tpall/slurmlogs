6829661a3f35bb1027da95ddb747695eb6c5e4e7
Taavi Päll <tapa741@gmail.com>
2018-11-19 23:26:38 +0200

    fix line regex

--- R/analyse.R
+++ R/analyse.R
@@ -11,14 +11,14 @@ unfinished_logs_nested <- unfinished_logs %>%
 
 finished_logs <- finished_logs %>% 
   group_by(path) %>% 
-  summarise(start = log[12],
+  summarise(start = log[grep("\\[",log)[1]],
             start = str_replace_all(start, "\\[|\\]", ""),
             end = log[grep("Finished job 0", log) - 1],
             end = str_replace_all(end, "\\[|\\]", ""),
-            rule = log[13],
+            rule = log[grep("\\[",log)[1] + 1],
             rule = str_replace_all(rule, "^rule |:$", ""),
-            input = log[grep("input", log)],
-            input = str_replace(input, "^input: ", "")) %>% 
+            sample = unique(str_extract(log[grep("input", log)], "SRR\\d+"))[1]) %>% 
+  .[-3759,] %>% 
   separate(start, c("wd", "m", "d", "t", "y"), sep = "\\s+") %>% 
   unite(start, "y", "m", "d", "t", sep = "-") %>% 
   select(-wd) %>% 
@@ -28,37 +28,67 @@ finished_logs <- finished_logs %>%
   mutate_at(vars(start, end), ymd_hms)
 
 runtime <- finished_logs %>% 
-  mutate(runtime = difftime(end, start, units = "mins"),
-         run = str_extract(input, "SRR\\d+")) %>% 
-  filter(runtime > 0)
+  mutate(runtime = difftime(end, start, units = "mins")) %>% 
+  filter(runtime > 0) %>% 
+  select(sample, start:runtime)
 
 runtime %>% 
-  filter(runtime > 11) %>% 
   ggplot() +
   geom_histogram(mapping = aes(x = runtime), bins = 30) + 
-  facet_wrap(~rule, scales = "free")
+  facet_wrap(~rule, scales = "free") +
+  scale_x_continuous()
 
 runtime %>% 
-  filter(runtime < 150, rule == "repeatmasker") %>%
+  filter(rule == "repeatmasker") %>%
   summarise_at("runtime", funs(min, mean, median, max))
 
-fastq <- read_csv("output/fastq_size.csv")
-fastq <- mutate(fastq, run = str_replace_all(dirname(path), "^[[:punct:]]+", ""))
+runs <- read_tsv("output/samples_remote.tsv")
 
-run_size <- select(runtime, run, rule, runtime) %>% 
-  inner_join(select(fastq, run, size))
+run_size <- inner_join(runtime, select(runs, sample, read_count, base_count, fq1_bytes)) %>% 
+  mutate(runtime = parse_number(runtime)) %>% 
+  distinct()
 
 run_size %>% 
-  ggplot(mapping = aes(x = size, y = runtime)) +
-  geom_point(aes(color = run), position = position_jitter(width = 0.1)) +
+  select(fq1_bytes) %>% 
+  distinct() %>% 
+  ggplot() +
+  geom_histogram(aes(fq1_bytes / 1048576))
+
+filter(run_size, (fq1_bytes / 1048576) < 500) %>% 
+  select(fq1_bytes) %>% 
+  distinct() %>% 
+  summarise_all(funs(mean, sd, max))
+
+
+run_size %>% 
+  ggplot(mapping = aes(x = fq1_bytes, y = runtime)) +
+  geom_point(aes(color = sample), position = position_jitter(width = 0.1)) +
   geom_smooth(method = "lm") +
-  facet_wrap(~rule, scales = "free") +
+  facet_wrap(~ rule, scales = "free") +
   scale_y_continuous() +
   scale_colour_discrete(guide = FALSE)
 
 run_size %>% 
-  group_by(rule, big = size > 6e8) %>% 
+  group_by(rule, big = fq1_bytes > 6e8) %>% 
   summarise_at("runtime", funs(mean, median, max))
+
+#' Fit models for each program
+models <- run_size %>% 
+  group_by(rule) %>% 
+  nest() %>% 
+  filter(map_lgl(data, ~nrow(.x) > 10)) %>% 
+  mutate(mod = map(data, ~broom::tidy(lm(runtime ~ fq1_bytes, data = .x))))
+
+models %>% 
+  select(rule, mod) %>% 
+  unnest() %>% 
+  select(rule, term, estimate) %>% 
+  spread(key = term, value = estimate) %>% 
+  rename_all(str_replace_all, "\\)|\\(", "") %>% 
+  filter(fq1_bytes > 0) %>% 
+  mutate(mb300 = Intercept + (300 * 1048576) * fq1_bytes,
+         mb3000 = Intercept + (3000 * 1048576) * fq1_bytes) %>% 
+  mutate_at(vars(starts_with("mb")), ~ .x / 60)
 
 #' ## Unfinished jobs
 #' 
